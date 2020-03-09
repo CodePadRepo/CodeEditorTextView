ff62524a9bfa01e089536ce5074d6a650bfad26c
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-09 23:07:35 +0900

    Add stub project for macOS PoC

---
+++ macos-poc/macos-poc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos-poc
+//
+//  Created by Ryang Sohn on 2020/03/09.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-poc/macos-poc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macos-poc
+//
+//  Created by Ryang Sohn on 2020/03/09.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
