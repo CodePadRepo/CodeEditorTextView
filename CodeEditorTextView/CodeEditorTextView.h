80772a4e2b9e7abd8a30e91e9599f0b9c509063e
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-10 23:07:45 +0900

    Re-create Xcode projects

---
+++ CodeEditorTextView/CodeEditorTextView.h
@@ -0,0 +1,19 @@
+//
+//  CodeEditorTextView.h
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/10.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CodeEditorTextView.
+FOUNDATION_EXPORT double CodeEditorTextViewVersionNumber;
+
+//! Project version string for CodeEditorTextView.
+FOUNDATION_EXPORT const unsigned char CodeEditorTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeEditorTextView/PublicHeader.h>
+
+
---
+++ CodeEditorTextViewPoC/CodeEditorTextViewPoC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeEditorTextViewPoC
+//
+//  Created by Ryang Sohn on 2020/03/10.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
