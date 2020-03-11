0afa24e24bea4047c3f080fb937334189eed720d
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-11 23:11:54 +0900

    Separate view controller and add LanguageDefinition

---
+++ CodeEditorTextView/Models/LanguageDefinition.swift
@@ -0,0 +1,15 @@
+//
+//  LanguageDefinition.swift
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/11.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import Foundation
+
+public struct LanguageDefinition {
+    var name: String
+    var keywords: [String]
+    var operators: [String]
+}
