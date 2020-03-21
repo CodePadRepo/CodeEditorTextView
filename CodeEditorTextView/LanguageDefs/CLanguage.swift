2e55decfd5638afed1a65f164707ddecfc89bf96
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-21 14:45:52 +0900

    Add LanguageDefinition and test C definition

---
+++ CodeEditorTextView/LanguageDefs/CLanguage.swift
@@ -0,0 +1,24 @@
+//
+//  CLanguage.swift
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/21.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+open class CLangaugeDef {
+    static let def = LanguageDefinition(name: "C", highlights: [
+        "auto",
+        "break",
+        "case",
+        "char",
+        "const",
+        "continue",
+        "default",
+        "do",
+        "double",
+        "else",
+        "enum",
+        "extern"
+    ])
+}
---
+++ CodeEditorTextView/Models/LanguageDefinition.swift
@@ -0,0 +1,12 @@
+//
+//  LanguageDefinition.swift
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/21.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+struct LanguageDefinition {
+    var name: String
+    var highlights: [String]
+}
