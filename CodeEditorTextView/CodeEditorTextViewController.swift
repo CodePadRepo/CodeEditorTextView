6ddcb26f4e2d56317a4a8a489a0cf74991df101d
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-21 11:40:18 +0900

    Revert "Separate view controller and add LanguageDefinition"
    This reverts commit 0afa24e24bea4047c3f080fb937334189eed720d.

---
+++ CodeEditorTextView/CodeEditorTextViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CodeEditorTextView.swift
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/10.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+import UIKit
+
+open class CodeEditorTextViewController: UIViewController, UITextViewDelegate {
+    @IBOutlet weak var codeEditorTextView: UITextView!
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        codeEditorTextView.delegate = self
+        codeEditorTextView.attributedText = NSAttributedString(string: "")
+    }
+    
+    public func textViewDidChange(_ textView: UITextView) {
+        let attributedString = NSMutableAttributedString(string: textView.text)
+        let words = textView.text.components(separatedBy: " ")
+        
+        for word in words {
+            if word == "highlight" {
+                let range = (attributedString.string as NSString).range(of: word)
+                attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.blue, range: range)
+            }
+        }
+        textView.attributedText = attributedString
+    }
+}
