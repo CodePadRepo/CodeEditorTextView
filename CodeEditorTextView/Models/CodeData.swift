71e56913872f3bc79b3c94ef58db777baedb5e75
Ryang Sohn <loop.infinitely@gmail.com>
2020-03-21 14:07:40 +0900

    Add CodeData and restructure source tree

---
+++ CodeEditorTextView/Models/CodeData.swift
@@ -0,0 +1,39 @@
+//
+//  CodeData.swift
+//  CodeEditorTextView
+//
+//  Created by Ryang Sohn on 2020/03/21.
+//  Copyright © 2020 Ryang Sohn. All rights reserved.
+//
+
+open class CodeData {
+    private(set) var lines: [String]
+    
+    public init(of codeText: String) {
+        lines = codeText.components(separatedBy: "\n") // TODO: Support other line endings
+    }
+    
+    public func editLine(lineNumber: Int, newLineContent: String) {
+        lines[lineNumber] = newLineContent
+    }
+    
+    public func insertNewLine(after newlinePos: Int) {
+        lines.insert("", at: newlinePos + 1)
+    }
+    
+    public func insertNewLine(before newlinePos: Int) {
+        lines.insert("", at: newlinePos)
+    }
+    
+    public func breakLine(_ linePos: Int, atPos breakIndex: String.Index) {
+        let beforeBreak = String(lines[linePos][..<breakIndex])
+        let afterBreak = String(lines[linePos][breakIndex...])
+        editLine(lineNumber: linePos, newLineContent: beforeBreak)
+        insertNewLine(after: linePos)
+        editLine(lineNumber: linePos + 1, newLineContent: afterBreak)
+    }
+    
+    public func dumpString() -> String {
+        return lines.joined(separator: "\n") // TODO: Support other line endings
+    }
+}
